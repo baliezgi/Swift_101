5ca6f3584f19448e7bcc8e544d26cad6da205510
Ezgi Bali <ezgibalici1@gmail.com>
2023-08-19 17:12:46 +0300

    Var and Let

---
+++ VarAndLet.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var greeting = "Hello, "
+var bosluk:String
+
+let pi=3.14
+let bosSabit:Int
+var isim = "Ezgi"
+
+greeting = "Merhaba,"
+
+isim = "Ali"
+print(greeting + isim)
+print("Hello, \(isim)")
+
+var yaslar = [3,5,7,13]
+var isimler = ["Ayşe","Mehmet","Cihat","Ahmet"]
+
+print("\(isimler[1]),\(yaslar[0]) yaşındadır.")
